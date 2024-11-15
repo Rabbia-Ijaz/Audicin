1c75b9c0dc894a34fe10b76f9ff6d801d8d225a5
Rabbia Ijaz <rabbiaijaz19@gmail.com>
2024-11-15 18:28:18 +0200

    Initial Commit

---
+++ Audicin/AudicinApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudicinApp.swift
+//  Audicin
+//
+//  Created by Rabbia Ijaz on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct AudicinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
